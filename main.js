2bd6f9112b923f369d9504286a2c8f3b079b3081
Abdulrehman <you@example.com>
2024-04-08 01:45:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+var friendsName = ["Moiz", "Yasir", "Abdullah", "Ali"];
+console.log(friendsName[0]);
+console.log(friendsName[1]);
+console.log(friendsName[2]);
+console.log(friendsName[3]);
---
+++ main.ts
@@ -0,0 +1,10 @@
+
+let friendsName: string [] = ["Moiz", "Yasir", "Abdullah", "Ali" ];  
+
+console.log(friendsName[0]);
+
+console.log(friendsName[1]);
+
+console.log(friendsName[2]);
+
+console.log(friendsName[3]);  
